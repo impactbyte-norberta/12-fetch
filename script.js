fd7a89954eafa97ec764849c836b97fe75ecb185
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-06-23 11:11:46 +0700

    Create check registered user

--- script.js
+++ script.js
@@ -13,17 +13,31 @@ async function register() {
             password,
         };
 
+        // Cek email sudah terdaftar atau belum
         let url = `https://5ef168bf1faf160016b4d5a9.mockapi.io/api/users`;
-        let options = {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        };
 
-        let response = await fetch(url, options);
-        let result = await response.json();
+        let response = await fetch(url);
+        let allUsers = await response.json();
+
+        let registeredUser = allUsers.filter((user) => user.email === email);
+
+        // Kalau sudah terdaftar
+        if (registeredUser.length > 0) {
+            alert(`Anda sudah terdaftar`);
+        } else {
+            // Kalau belum terdaftar, lakukan registrasi
+            let options = {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            };
+            let response = await fetch(url, options);
+            let result = await response.json();
+
+            alert(`Terima kasih sudah mendaftar`);
+        }
     } catch (error) {
         console.error(error);
     }
